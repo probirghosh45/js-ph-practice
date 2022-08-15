33c48a177af9c4e32c74b35c691bddce34e6b9e5
probir ghosh <probirghosh@outlook.com>
2022-08-15 07:03:28 +0600

    practiced

---
+++ bestFriend.js
@@ -0,0 +1,29 @@
+const friendshipOne = {
+    selfName : "Rahim" ,
+    friendName : "Karim"
+}
+
+
+
+// const friendshipTwo = ["Shakil"]
+// console.log(typeof(friendshipTwo))  //object ? validation ?
+
+const friendshipTwo = {
+    selfName : "Karim" ,
+    friendName : "Rahim"
+}
+
+
+const bestFriend = (type1,type2)=> {
+    if(typeof(type1) == "object" && typeof(type2) =="object"){
+        // return "OK";
+        if(type1.selfName.toLowerCase() == type2.friendName.toLowerCase() && type2.friendName.toLowerCase() ==  type1.selfName.toLowerCase()){
+          return "They are Best Friend";
+        }
+        return "They are not Best Friend";
+    }
+    else {
+        return "Pls Input Object Type Data"
+    }
+}
+console.log(bestFriend(friendshipOne,friendshipTwo));
